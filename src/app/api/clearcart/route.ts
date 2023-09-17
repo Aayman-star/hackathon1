24ab4682e09017f54fad3198cf78f12a5b3359a6
Aayman-star <aayman.khalid1@gmail.com>
2023-09-17 19:23:41 +0500

    functioning store

---
+++ src/app/api/clearcart/route.ts
@@ -0,0 +1,34 @@
+/**METHOD TO DELETE DATA FROM THE DATABASE */
+import { NextRequest,NextResponse } from "next/server";
+import { db,cartTable } from "@/lib1/drizzle";
+import { eq,and, asc } from "drizzle-orm";
+export const DELETE = async(request: NextRequest)=>
+{
+  const req = request.nextUrl;
+  //const productId =  req.searchParams.get('product_id') as string;
+  const userId = req.searchParams.get('user_id') as string;
+    
+    try{
+        if(userId){
+            console.log(`PUSER_ID : ${userId} from the clear cart api`)
+            const res = await db.delete(cartTable).where(eq(cartTable.user_id,userId)).returning();
+
+            return NextResponse.json({Message: `Data deleted successfully`},{status:200})
+        }
+        else{
+            // // if('product_id'){
+            //     throw new Error(`Login required to procees`)
+            // }
+            // else{
+            //     throw new Error(`Product not found`)
+            // }
+                    console.log(`USER ID MISSING`)
+        }
+    }
+    catch(error){
+        console.log(error)
+        return NextResponse.json({Message:`Failed to remove data`},{status:405})
+
+    }
+
+}
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { getCookie } from "cookies-next";
+
+const clearCart = async (userId: string) => {
+  try {
+    await fetch(`/api/clearcart?user_id=${userId}`, {
+      method: "DELETE",
+    });
+  } catch (error) {
+    console.log(`Something went wrong: `, error);
+  }
+};
 
 const Page = () => {
+  const userId = getCookie("user_id") as string;
+  console.log("USER ID IN THE SUCCESS PAGE----", userId);
+  useEffect(() => {
+    clearCart(userId);
+  }, []);
   return (
     <>
-      <div className="w-full h-screen lg:max-w-7xl lg:mx-auto grid place-content-center">
+      <div className="w-full h-screen p-4 mt-2 lg:max-w-7xl lg:mx-auto lg:grid lg:place-content-center">
         <p className="bg-white text-gray-900 text-5xl font-semibold">
           Order Placed Successfully...
         </p>
