5eec82521b4a185ee7508b1d0d43faac9cbf57bf
Aayman-star <aayman.khalid1@gmail.com>
2024-09-16 21:53:01 +0500

    fixed the products display for larger screens

--- src/app/Products/[label]/page.tsx
+++ src/app/Products/[label]/page.tsx
@@ -16,7 +16,7 @@ const Page = async ({ params }: { params: { label: string } }) => {
 
   return (
     <div className="bg-white min-h-screen w-full p-2 grid place-content-center">
-      <div className="w-full md:max-w-7xl md:mx-auto min-h-screen  px-8 py-6 flex flex-wrap justify-start gap-8">
+      <div className="w-full md:max-w-6xl md:mx-auto min-h-screen p-8 flex flex-wrap justify-start gap-8">
         {productsList?.map((product, i) => (
           <div key={i}>
             <DisplayProducts {...product} index={i} />
