86c49665eaef4e4c15d4c6b1e0d74c25c05a4547
Aayman-star <aayman.khalid1@gmail.com>
2024-09-18 12:55:48 +0500

    removed the clearCart function from checkout

--- src/app/Cart/CheckOutBtn.tsx
+++ src/app/Cart/CheckOutBtn.tsx
@@ -33,7 +33,7 @@ const CheckOutBtn = () => {
       // console.log(`DATA IN CHECKOUT:`, data.session);
       if (data.session) {
         stripe?.redirectToCheckout({ sessionId: data.session.id });
-        dispatch(cartActions.ClearCart());
+        // dispatch(cartActions.ClearCart());
       }
     }
   };
